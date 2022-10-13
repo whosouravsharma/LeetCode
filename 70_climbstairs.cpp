1a6ae17452c14478c4e4b068b8f7322073719d54
whosouravsharma <whosouravsharma@gmail.com>
2022-10-13 23:44:47 +0530

    13-10-22

--- 70_climbstairs.cpp
+++ 70_climbstairs.cpp
@@ -11,26 +11,45 @@ Explanation: There are two ways to climb to the top.
 2. 2 steps */
 
 #include<iostream>
+#include<vector>
 
-int climbstairs(int n){
-    int *dp=new int();
-    dp[1]=1;
-    dp[2]=2;
-    for(int i=3;i<=n;i++){
-        dp[i]=dp[i-1]+dp[i-2];
-    }
-    return dp[n];;
-    dp[2]=2;
-    for(int i=3;i<=n;i++){
-        dp[i]=dp[i-1]+dp[i-2];
-    }
-    return dp[n];
-}
+// DP:
+// int climbstairs(int n){
+//     int *dp=new int();
+//     dp[1]=1;
+//     dp[2]=2;
+//     for(int i=3;i<=n;i++){
+//         dp[i]=dp[i-1]+dp[i-2];
+//     }
+//     return dp[n];;
+//     dp[2]=2;
+//     for(int i=3;i<=n;i++){
+//         dp[i]=dp[i-1]+dp[i-2];
+//     }
+//     return dp[n];
+// }
+
+// Memoization:
+
+class Solution{
+    public:
+        int climbstairs(int n){
+            std::vector<int> memo(n+1,-1);
+            return count(n,memo);
+        }
+        int count(int n, std::vector<int> &memo){
+            if(n<0) return 0;
+            if(memo[n]!= -1) return memo[n];
+            if(n==0) return 1;
+            return memo[n]=count(n-1,memo) + count(n-2, memo);
+        }
+};
 
 int main(){
     int num;
     std::cin>>num;
-    int res=climbstairs(num);
+    Solution s;
+    int res=s.climbstairs(num);
     std::cout<<res<<std::endl;
     return 0;
 }
